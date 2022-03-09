8edf92d639bd480af72abfd994c9fab941feb06e
Jesse Salinas <jessesalinas53@yahoo.com>
2022-03-09 14:22:37 -0600

    MergeConflicts

--- Assets/Scripts/Minigame Scripts/Swipe Game/Swipe.cs
+++ Assets/Scripts/Minigame Scripts/Swipe Game/Swipe.cs
@@ -20,14 +20,14 @@ public class Swipe : MonoBehaviour
 
     void Update()
     {
-        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began && canSwipe)
+        if(Input.GetMouseButtonDown(0)) //&& Input.GetTouch(0).phase == TouchPhase.Began && canSwipe)
         {
             touchTimeStart = Time.time;
             startPos = Input.GetTouch(0).position;
-            canSwipe = false;
+            //canSwipe = false;
         }
 
-        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        if(Input.GetMouseButtonUp(0)) //&& Input.GetTouch(0).phase == TouchPhase.Ended)
         {
             touchTimeFinish = Time.time;
             touchInterval = touchTimeFinish - touchTimeStart;
